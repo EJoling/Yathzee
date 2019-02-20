d8856e9b27f901c19722c7946743a73ffa91dfd3
EJoling <emieljoling@gmail.com>
2019-02-20 11:16:57 +0100

    Punt 1: mogelijk om te blijven spelen

--- Yathzee.java
+++ Yathzee.java
@@ -3,7 +3,25 @@ import java.util.Scanner;
 public class Yathzee {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		
+		int stoppen = 0;
+		
+		while(stoppen == 0) {
+			System.out.println("Druk op Enter om te spelen, q om te stoppen");
+			Scanner scanner = new Scanner(System.in);
+			String input = scanner.nextLine();	
+			
+				switch(input) {
+				case "":
+					System.out.println("Yay werkt");
+					break;
+				case "q":
+					System.out.println("Spel is gestopt");
+					stoppen ++;
+					break;
+			
+			}//end switch
+		}//end while
 		
 	}//end main
 	
