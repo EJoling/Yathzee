4d93da621a61902e7754e42015b4d4a0e85826ff
EJoling <emieljoling@gmail.com>
2019-02-22 15:16:38 +0100

    4e doelstelling: een spel start met 5 dobbelstenen in de hand die geworpen kunnen worden. Elke dobbelsteen heeft dan zijn eigen random waarde

--- Yathzee.java
+++ Yathzee.java
@@ -1,3 +1,15 @@
+/**
+ * Yathzeespel waarbij O.O.P. een vereiste is. 
+ * Bij het aanmaken van een nieuw spel worden er 5 dobbelstenen aangemaakt, waarmee geworpen kan worden. 
+ * 
+ * 
+ * 
+ * 
+ * @author Emiel
+ */
+
+
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,23 +21,38 @@ public class Yathzee {
 		spel.spelen();
 		
 		
+		
 	}//end main	
 }//end class Yathzee
 
 class YahtzeeSpel {
+	// Elk spel start met 5 dobbelstenen
+	static ArrayList<Dobbelsteen> Dobbelstenen = new ArrayList<>();
 	
+	YahtzeeSpel(){
+			for(int i =0; i<5; i++) {
+			Dobbelsteen dobbelsteen = new Dobbelsteen();
+			Dobbelstenen.add((i), dobbelsteen);
+			}//end for loop		
+	}//end constructor YahtzeeSpel
 	
-	void spelen() {
+	static void spelen() {
 		int stoppen = 0;
 		
 		while(stoppen == 0) {
 			System.out.println("Druk op Enter om te spelen, q om te stoppen");
 			Scanner scanner = new Scanner(System.in);
 			String input = scanner.nextLine();	
-			
+			 //	Een speler kan spelen(werpen) of het spel stoppen  
 				switch(input) {
 				case "":
-					System.out.println("Yay werkt");
+					System.out.println("De worp is:");
+					for (Dobbelsteen steen:Dobbelstenen)
+						System.out.print(steen.werpen());
+					System.out.println("");
+						System.out.println("Plek 0 : " + Dobbelstenen.get(0));
+						System.out.println("Plek 0 waarde: " + Dobbelstenen.get(0).aantalOgen);
+					
 					break;
 				case "q":
 					System.out.println("Spel is gestopt");
@@ -36,10 +63,20 @@ class YahtzeeSpel {
 		}//end while
 	}//end methode spelen
 	
+	
+class Dobbelsteen {
+	
+	int aantalOgen;
+	
 	int werpen(){
 		Random random = new Random();
-		int worp = random.nextInt(6)+1;
-		return worp;
+		aantalOgen = random.nextInt(6)+1;
+		return aantalOgen;
+		
+		
+		
 	}//end werpen
+	
+}//end class Dobbelsteen
 	
 }//end class YahtzeeSpel
